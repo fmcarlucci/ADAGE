37a9fe6cd54b2de1fad0011cd50bf2265af3fffc
enoonIT <enoonIT@users.noreply.github.com>
2018-03-03 17:52:14 +0100

    misc

--- models/model.py
+++ models/model.py
@@ -1,5 +1,20 @@
 import torch.nn as nn
-from functions import ReverseLayerF
+from torch.autograd import Function
+
+
+class ReverseLayerF(Function):
+
+    @staticmethod
+    def forward(ctx, x, alpha):
+        ctx.alpha = alpha
+
+        return x.view_as(x)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        output = grad_output.neg() * ctx.alpha
+
+        return output, None
 
 
 class CNNModel(nn.Module):
