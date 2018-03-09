046caf20399956fc1e64c8d03f08d13546732243
enoonIT <enoonIT@users.noreply.github.com>
2018-03-09 08:19:24 +0100

    renamed param

--- new_main.py
+++ new_main.py
@@ -30,7 +30,7 @@ def get_args():
     parser.add_argument('--train_deco_weight', action="store_true")
     parser.add_argument('--deco_blocks', default=4, type=int)
     parser.add_argument('--deco_kernels', default=64, type=int)
-    parser.add_argument('--deco_block', default='basic', choices=deco_types.keys(),
+    parser.add_argument('--deco_block_type', default='basic', choices=deco_types.keys(),
                         help="Which kind of deco block to use")
     parser.add_argument('--deco_output_channels', type=int, default=3, help="3 or 1")
     parser.add_argument('--suffix', help="Will be added to end of name", default="")
@@ -45,7 +45,7 @@ def get_name(args, seed):
     name = "lr:%g_BS:%d_epochs:%d_DannW:%g_IS:%d" % (args.lr, args.batch_size, args.epochs,
                                                      args.DANN_weight, args.image_size)
     if args.use_deco:
-        name += "_deco%d_%d_%s_%dc" % (args.deco_blocks, args.deco_kernels, args.deco_block, args.deco_output_channels)
+        name += "_deco%d_%d_%s_%dc" % (args.deco_blocks, args.deco_kernels, args.deco_block_type, args.deco_output_channels)
     if args.train_deco_weight:
         name += "_trainWeight"
     if args.classifier:
@@ -110,7 +110,7 @@ dataloader_target = torch.utils.data.DataLoader(
 
 if args.use_deco:
     my_net = Combo(n_deco=args.deco_blocks, classifier=args.classifier, train_deco_weight=args.train_deco_weight,
-                   deco_kernels=args.deco_kernels, deco_block=deco_types[args.deco_block], out_channels=args.deco_output_channels)
+                   deco_kernels=args.deco_kernels, deco_block=deco_types[args.deco_block_type], out_channels=args.deco_output_channels)
 else:
     my_net = get_classifier(args.classifier)
 
