647fa545a3469d52647bb0ea66550c727632a5bc
enoonIT <enoonIT@users.noreply.github.com>
2018-03-15 17:24:47 +0100

    added synth

--- dataset/data_loader.py
+++ dataset/data_loader.py
@@ -4,14 +4,19 @@ from PIL import Image
 import os
 from torchvision import datasets
 from torchvision import transforms
+import numpy as np
 
 mnist = 'mnist'
 mnist_m = 'mnist_m'
 svhn = 'svhn'
+synth = 'synth'
+webcam = "webcam"
+amazon = "amazon"
 mnist_image_root = os.path.join('dataset', 'mnist')
 mnist_m_image_root = os.path.join('dataset', 'mnist_m')
+synth_image_root = os.path.join('dataset', 'SynthDigits')
 
-dataset_list = [mnist, mnist_m, svhn]
+dataset_list = [mnist, mnist_m, svhn, synth, amazon, webcam]
 
 
 def get_dataset(name, image_size, mode="train"):
@@ -64,9 +69,16 @@ def load_dataset(img_transform, dataset_name):
             data_list=train_list,
             transform=img_transform
         )
-    elif dataset_name == "amazon":
+    elif dataset_name == synth:
+        train_mat = os.path.join(synth_image_root, 'synth_train_32x32.mat')
+        dataset = GetSynthDigits(
+            data_root=synth_image_root,
+            data_mat=train_mat,
+            transform=img_transform
+        )
+    elif dataset_name == amazon:
         dataset = datasets.ImageFolder('dataset/amazon', transform=transforms)
-    elif dataset_name == "webcam":
+    elif dataset_name == webcam:
         dataset = datasets.ImageFolder('dataset/webcam', transform=transforms)
     elif type(dataset_name) is list:
         dataset = ConcatDataset([load_dataset(img_transform, dset) for dset in dataset_name])
@@ -103,6 +115,35 @@ class GetLoader(data.Dataset):
 
     def __len__(self):
         return self.n_data
+
+
+class GetSynthDigits(data.Dataset):
+    def __init__(self, data_root, data_mat, transform=None):
+        self.root = data_root
+        self.transform = transform
+
+        import scipy.io as sio
+
+        # reading(loading) mat file as array
+        loaded_mat = sio.loadmat(data_mat)
+        self.data = loaded_mat['X']
+        self.data = np.transpose(self.data, (3, 2, 0, 1))
+        self.labels = loaded_mat['y'].astype(np.int64).squeeze()
+
+    def __getitem__(self, item):
+        img, labels = self.data[item], self.labels[item]
+        # doing this so that it is consistent with all other datasets
+        # to return a PIL Image
+        img = Image.fromarray(np.transpose(img, (1, 2, 0)))
+
+        if self.transform is not None:
+            img = self.transform(img)
+            labels = int(labels)
+
+        return img, labels
+
+    def __len__(self):
+        return len(self.data)
 
 
 def get_dataloader(dataset_name, batch_size, image_size, mode):
