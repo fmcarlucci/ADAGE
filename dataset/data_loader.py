0d12afdb70d0075d46755d65e061db0c789d2e7c
enoonIT <enoonIT@users.noreply.github.com>
2018-03-15 18:56:35 +0100

    fix by Paolo

--- dataset/data_loader.py
+++ dataset/data_loader.py
@@ -27,7 +27,7 @@ def get_dataset(name, image_size, mode="train"):
             transforms.ToTensor(),
             transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))
         ])
-    elif mode == "train":
+    elif mode == "office":
         img_transform = transforms.Compose([
             transforms.RandomResizedCrop(image_size),
             transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=2),
